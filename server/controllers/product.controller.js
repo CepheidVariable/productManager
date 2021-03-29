d896ef3a1632ea1f2239e8d42fc2fd9c2ff9c844
CepheidVariable <robertwoodard.usa@gmail.com>
2021-03-29 15:39:33 -0500

    product controller added

---
+++ server/controllers/product.controller.js
@@ -0,0 +1,33 @@
+const Product = require('../models/product.model');
+
+module.exports = {
+    index: (req, res) => {
+        Product.find()
+            .then(data => res.json({results: data}))
+            .catch(err => res.status(404).json({errors: err.errors}))
+    },
+    show: (req, res) => {
+        Product.find({_id: req.params.id})
+            .then(data => res.json({results: data}))
+            .catch(err => res.status(404).json({errors: err.errors}))
+    },
+    create: (req, res) => {
+        Product.create(req.body)
+            .then(data => res.json({results: data}))
+            .catch(err => res.status(404).json({errors: err.errors}))
+    },
+    update: (req, res) => {
+        Product.findOneAndUpdate(
+            { _id: req.params.id },             //filter
+            req.body,                           //replacement
+            { new: true, runValidators: true }  //options
+        )
+            .then(data => res.json({results: data}))
+            .catch(err => res.status(404).json({errors: err.errors}))
+    },
+    delete: (req, res) => {
+        Product.delete({_id: req.params.id})
+            .then(data => res.json({results: data}))
+            .catch(err => res.status(404).json({errors: err.errors}))
+    }
+};
