5566b1f297ddf3c8a5f6672e3646bb22a4042a1c
CepheidVariable <robertwoodard.usa@gmail.com>
2021-03-29 14:59:46 -0500

    setup of server.js

--- server.js
+++ server.js
@@ -3,3 +3,11 @@ const express = require('express'),
     port = 8000,
     server = app.listen(port, () => console.log(`Listening on port ${port}...`))
     cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({"extended":true}));
+
+// representation of our DB config file
+
+// representation of routes file export - annonymous function
