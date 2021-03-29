360079d9f7e3675fab7a8911b378f8a157ee688b
CepheidVariable <robertwoodard.usa@gmail.com>
2021-03-29 15:44:58 -0500

    server config added

---
+++ server/configs/database.config.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose'),
+    db = "products_manager";
+
+mongoose.connect(`mongodb://localhost/${db}`, {
+    useNewUrlParser = true,
+    useUnifiedTopology = true
+})
+    .then(() => console.log(`Success: MongoDB '${db}' connected`))
+    .catch(err => console.log("Warning: database connection failed!", err));
