4e16e03c25600177990bbfcda82280aaa2c60dac
CepheidVariable <robertwoodard.usa@gmail.com>
2021-03-29 17:03:24 -0500

    form formatting

--- client/src/components/ProductForm.jsx
+++ client/src/components/ProductForm.jsx
@@ -15,20 +15,23 @@ export default props =>{
         <form onSubmit={onSubmitHandler} className="col-6 mt-5">
             <div className="form-group row">
                 <label htmlFor="title" className="col-sm-3 col-form-label">Title:</label>
-                <div className="col-sm-2">
+                <div className="col-sm-9">
                     <input className="form-control" name="title" onChange={ e => setTitle(e.target.value) } />
                 </div>
-            </div>
+            </div>           
             <div className="form-group row">
                 <label htmlFor="price" className="col-sm-3 col-form-label">Price:</label>
-                <div className="col-sm-2">
+                <div className="col-sm-3 input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text">$</span>
+                    </div>
                     <input className="form-control" name="price" onChange={ e => setPrice(e.target.value) } />
                 </div>
             </div>
             <div className="form-group row">
                 <label htmlFor="description" className="col-sm-3 col-form-label">Description:</label>
-                <div className="col-sm-2">
-                    <input className="form-control" name="description" onChange={ e => setDescription(e.target.value) } />
+                <div className="col-sm-9">
+                    <textarea className="form-control" name="description" onChange={ e => setDescription(e.target.value) } />
                 </div>
             </div>
             <div className="form-group row">
