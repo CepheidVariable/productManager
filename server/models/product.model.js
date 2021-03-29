4b22bcfd0aa35b8fe1bef43d5e5621e1639eeb71
CepheidVariable <robertwoodard.usa@gmail.com>
2021-03-29 15:22:23 -0500

    product model added

---
+++ server/models/product.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, "Product must have a title!"],
+        minlength: [2, "Title must be at least two characters in length!"]
+    },
+    price: {
+        type: Decimal128,
+        required: [true, "Item must have a price!"],
+        min: [0, "Price must be greater than $0.00!"]
+    },
+    description:{
+        type: String,
+        required: [true, "Item must have a description!"],
+        minlength: [8, "Description must be at least 8 characters in length!"]
+    }
+});
+
+const Product = new mongoose.model('Product', ProductSchema );
+module.exports = Product;
