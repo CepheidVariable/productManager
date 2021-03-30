cefba865308b09edfbdd25d5cb46b54f22c330bb
CepheidVariable <robertwoodard.usa@gmail.com>
2021-03-29 22:10:37 -0500

    more refactoring using falsy rendering

--- client/src/components/Details.jsx
+++ client/src/components/Details.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react"
 
 export default ({id}) => {
     const [product, setProduct] = useState({});
-    const [isLoaded, setIsLoaded] = useState(false);
+    // const [isLoaded, setIsLoaded] = useState(false);
 
     // Create number formatter
     var formatter = new Intl.NumberFormat('en-US', {
@@ -14,14 +14,14 @@ export default ({id}) => {
     useEffect( ()=> {
         axios.get('http://localhost:8000/api/products/' + id)
             .then(res => setProduct(res.data.results[0]));
-            setIsLoaded(true);
+            // setIsLoaded(true);
     }, []);
 
     return (
         <div className="text-center mt-5">
-            <p><strong>{isLoaded && product.title}</strong></p>
-            <p>{isLoaded && product.description}</p>
-            <p>{isLoaded && formatter.format(product.price)}</p>
+            <p><strong>{product && product.title}</strong></p>
+            <p>{product && product.description}</p>
+            <p>{product && formatter.format(product.price)}</p>
         </div>
     )
 }
